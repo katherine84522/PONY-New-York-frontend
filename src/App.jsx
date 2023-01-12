b03f23a73e069c8cb4a764497acc8d3de340ca9a
colterlevi <cllongshore@gmail.com>
2023-01-12 15:11:07 -0500

    import components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './Login'
+import OngoingRequest from './OngoingRequests'
+import OpenRequest from './OpenRequests'
+import Signup from './SignUp'
+import RequestForm from './RequestForm'
+import Schedule from './schedule'
 
 const App = () => {
   return (
