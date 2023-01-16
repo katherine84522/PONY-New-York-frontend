7baeebdd2d838628e888e3fb7a9f3da410bbcf56
katherine84522 <katherine84522@gmail.com>
2023-01-16 17:05:38 -0500

    added google maps directions

--- src/OngoingRequests.jsx
+++ src/OngoingRequests.jsx
@@ -1,11 +1,32 @@
 import ProtectorNavBar from './ProtectorNavBar'
 import { useState, useEffect } from 'react'
+import {
+    useJsApiLoader,
+    GoogleMap,
+    Marker,
+    DirectionsRenderer,
+} from '@react-google-maps/api'
+
+const libraries = ['places']
 
 const OngoingRequest = () => {
     const [coords, setCoords] = useState({});
+    const [directionsResponse, setDirectionsResponse] = useState(null)
+    const [distance, setDistance] = useState('')
+    const [duration, setDuration] = useState('')
+    const [travelMode, setTravelMode] = useState('WALKING')
+    const [instructions, setInstructions] = useState([])
+
+
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY,
+        libraries: libraries,
+    })
+
+    // const regex = /(<([^>]+)>)/gi
 
     useEffect(() => {
-        const getLocation = () => {
+        const getLocation = async () => {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(position => {
                     setCoords({
@@ -17,17 +38,68 @@ const OngoingRequest = () => {
                 console.log("Geolocation is not supported by this browser.");
             }
         }
+        if (isLoaded) {
+            async function calculateRoute() {
+                const directionsService = new google.maps.DirectionsService()
+                directionsService.route({
+                    origin: { lat: coords.lat, lng: coords.lng },
+                    destination: "Penn Staion",
+                    travelMode: google.maps.TravelMode[travelMode],
+                }, (result, status) => {
+                    if (status === 'OK') {
+                        setDirectionsResponse(result);
+                        setDistance(result.routes[0].legs[0].distance.text)
+                        setDuration(result.routes[0].legs[0].duration.text)
+                        setInstructions(result.routes[0].legs[0].steps.map((step, index) => (
+                            <p key={index} dangerouslySetInnerHTML={{ __html: step.instructions }} />
+                        )))
+                    } else {
+                        console.log('Directions request failed due to ' + status);
+                    }
+                });
+            }
+            calculateRoute()
+        }
         getLocation()
-    }, [])
+    }, [isLoaded, travelMode])
 
 
-    return (
+
+
+
+    return isLoaded ? (
         <div>
             < ProtectorNavBar />
-            <p>Latitude: {coords.lat}</p>
-            <p>Longitude: {coords.lng}</p>
+            <p>Distance: {distance} </p>
+            <p>Duration: {duration} </p>
+            <button onClick={() => { setTravelMode('WALKING') }}>Walking</button>
+            <button onClick={() => { setTravelMode('TRANSIT') }}>Transit</button>
+            <GoogleMap
+                center={coords}
+                zoom={15}
+                mapContainerStyle={{ width: '100%', height: '30vh' }}
+                options={{
+                    zoomControl: false,
+                    streetViewControl: false,
+                    mapTypeControl: false,
+                    fullscreenControl: false,
+                    zoom: 13
+                }}
+                onLoad={map => {
+                    const bounds = new window.google.maps.LatLngBounds();
+                    map.fitBounds(bounds);
+                }}
+            >
+                <Marker position={coords} />
+                {directionsResponse && (
+                    <DirectionsRenderer directions={directionsResponse} />
+                )}
+            </GoogleMap>
+            <div>{instructions}</div>
         </div>
-    );
+    ) : <></>
+
+
 }
 
 export default OngoingRequest
