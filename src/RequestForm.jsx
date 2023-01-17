b7191427a68d4161bfd17738a60008572d778561
Shivani Jetty <shivanijetty@gmail.com>
2023-01-17 09:55:25 -0500

    added signup components

--- src/RequestForm.jsx
+++ src/RequestForm.jsx
@@ -1,13 +1,20 @@
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
 import WalkerNavBar from './WalkerNavBar'
 
 const RequestForm = () => {
+    const [start, setStart] = useState("")
+    const [end, setEnd] = useState("")
+    const [message, setMessage] = useState("")
 
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
         navigate('/walkerongoingrequest')
+
+
+
     }
     return (
         <>
@@ -15,7 +22,17 @@ const RequestForm = () => {
             <div>
                 <h2>Request a Protector</h2>
                 <form onSubmit={(e) => { handleSubmit(e) }}>
-                    <input type="submit" />
+                    {
+                        <div>
+                            <label for="start-location">START:</label>
+                            <input onChange={(e) => { setStart(e.target.value) }}type="text" id="start-location" /><br />
+                            <label for="end-location">END:</label>
+                            <input onChange={(e) => { setEnd(e.target.value) }}type="text" id="end-location"/><br />
+                            <label for="message"></label>
+                            <input onChange={(e) => { setMessage(e.target.value) }} type="text" placeholder=""/><br />
+                            <input type="submit" />
+                        </div>                        
+                    }
                 </form>
             </div>
         </>
