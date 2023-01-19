9c3821d71ca50051524e7cfeb26980a510e8792b
katherine84522 <katherine84522@gmail.com>
2023-01-19 12:48:58 -0500

    fixed ongoing and did css for schedule page

--- src/OpenRequests.jsx
+++ src/OpenRequests.jsx
@@ -21,7 +21,15 @@ const OpenRequests = ({ setOngoingRequest, ongoingRequest, coords }) => {
 
     const navigate = useNavigate()
     // useEffect(() => {
+    // useEffect(() => {
 
+    //     if (ongoingRequest) {
+    //         if (ongoingRequest.current) {
+    //             navigate('/OngoingRequest')
+    //         } else {
+    //             navigate('/protectorscheduledwalk')
+    //         }
+    //     }
     //     if (ongoingRequest) {
     //         if (ongoingRequest.current) {
     //             navigate('/OngoingRequest')
@@ -31,13 +39,14 @@ const OpenRequests = ({ setOngoingRequest, ongoingRequest, coords }) => {
     //     }
 
     // }, [ongoingRequest])
+    // }, [ongoingRequest])
 
     return (
         <div className='flex bg-hero bg-cover h-screen w-screen'>
             <div className='text-left'>
                 < ProtectorNavBar />
             </div>
-            <div className="flex-row pl-60 w-screen justify-center items-center p-10 backdrop-blur-sm overflow-auto">
+            <div className="flex-row pl-60 w-screen justify-center items-center p-10 backdrop-blur-sm">
                 {
                     requests.map((request, i) => {
                         return (
