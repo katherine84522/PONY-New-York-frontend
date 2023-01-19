2d0f341639e8d01d3cb07b8a276a9b6fb8582e76
colterlevi <cllongshore@gmail.com>
2023-01-19 11:55:38 -0500

    open request card css styling

--- src/OpenRequests.jsx
+++ src/OpenRequests.jsx
@@ -37,7 +37,7 @@ const OpenRequests = ({ setOngoingRequest, ongoingRequest, coords }) => {
             <div className='text-left'>
                 < ProtectorNavBar />
             </div>
-        <div className="flex-row pl-60 w-screen justify-center items-center p-10 backdrop-blur-sm">
+        <div className="flex-row pl-60 w-screen justify-center items-center p-10 backdrop-blur-sm overflow-auto">
             {
                 requests.map((request, i) => {
                     return (
